7a4af450ee4846b3f00e14438e520a00c67d6607
unknown <namitlodh>
2024-03-20 23:53:24 +0530

    [Namit] Add: toolbar module in keep notes

---
+++ src/app/Component/keep/keep.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keep',
+  templateUrl: './keep.component.html',
+  styleUrl: './keep.component.scss'
+})
+export class KeepComponent {
+
+}
