677f6c99f587a0f36f4fb07e56faa26890ae2f14
unknown <namitlodh>
2024-03-20 23:36:26 +0530

    [Namit] Add: Forgotpassword module

--- src/app/Component/forgotpassword/forgotpassword.component.ts
+++ src/app/Component/forgotpassword/forgotpassword.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../Services/userService/user.service';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -8,12 +9,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ForgotpasswordComponent implements OnInit{
   forgorpasswordForm!:FormGroup;
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder, private userService: UserService){}
+  
   ngOnInit(): void {
     this.forgorpasswordForm=this.formBuilder.group(
       {
         email: ['',[Validators.required,Validators.email]]
       }
+    )
+  }
+
+  ForgetSubmit(){
+    let reqData={
+      email: this.forgorpasswordForm.value.email
+    }
+    //console.log(reqData);
+    this.userService.forgotpassword(reqData).subscribe((res:any)=>{
+      console.log(res);
+    }
     )
   }
 }
